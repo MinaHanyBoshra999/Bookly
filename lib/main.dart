c65a46bed442397dee361a21e104a0a96a73c9ab
mina <sciencecomputer555@gmail.com>
2023-06-24 14:46:25 +0300

    constant commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       home:SplashView(),
     );
   }
